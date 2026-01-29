02f9fcab69fee1e099f428d98895ae803e38a5e2
Jesse Nickles <jesse@littlebizzy.com>
2026-01-30 02:47:22 +0700

    Create maintenance-mode.php

---
+++ maintenance-mode.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Maintenance Mode
+Plugin URI: https://www.littlebizzy.com/plugins/maintenance-mode
+Description: Minimalistic plugin with a simple one-click on/off switch and zero settings to worry about, featuring default WordPress styling without any images.
+Version: 2.0.0
+Author: LittleBizzy
+Author URI: https://www.littlebizzy.com
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+PBP Version: 1.2.0
+WC requires at least: 3.3
+WC tested up to: 3.5
+*/
